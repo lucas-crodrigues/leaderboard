2ecb2d7b736f31bbe0d7f0af3cc0b6ec2641fa56
lucascr <lcrodrigues1995@gmail.com>
2022-07-21 13:50:37 -0300

    delete unused code

--- src/modules/ui.js
+++
@@ -1,8 +0,0 @@
-import getScores from './getScores.js';
-
-const renderScores = () => {
-  const scoresUl = document.querySelector('ul');
-  scoresUl.innerHTML = getScores();
-};
-
-export default renderScores;
