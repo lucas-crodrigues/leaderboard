970b3dac0495f4888ad943f90155938bf8abeaf3
lucascr <lcrodrigues1995@gmail.com>
2022-07-21 13:48:41 -0300

    Call getScores and submitScore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import './styles.css';
 
-import renderScores from './modules/ui.js';
+import submitScore from './modules/submitScore';
 
-window.addEventListener('DOMContentLoaded', () => renderScores());
+import getScores from './modules/getScores.js';
+
+window.addEventListener('DOMContentLoaded', () => getScores());
+
+document.querySelector('.score-head button').addEventListener('click', () => {
+  document.querySelector('ul').innerHTML = '';
+  getScores();
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  submitScore();
+  form.reset();
+});
